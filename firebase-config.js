8c357c466cc4149a72648fefe7ae9fc0ae1132f6
johan-kadez <dafakurniawan1221@gmail.com>
2026-08-16 16:37:37 +0700

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,9 +1,9 @@
 // Gunakan config Web App Firebase yang sama dengan Store.
 export const firebaseConfig = {
-  apiKey: "REPLACE_ME",
-  authDomain: "REPLACE_ME",
-  projectId: "REPLACE_ME",
-  storageBucket: "REPLACE_ME",
-  messagingSenderId: "REPLACE_ME",
-  appId: "REPLACE_ME"
+  apiKey: "AIzaSyBbuVzR0657lZaTdaLKn9vUOO0diFs5kcw",
+  authDomain: "player-dashboard-b016a.firebaseapp.com",
+  projectId: "player-dashboard-b016a",
+  storageBucket: "player-dashboard-b016a.firebasestorage.app",
+  messagingSenderId: "627678288754",
+  appId: "1:627678288754:web:7f7e77f84b836fab211beb"
 };
