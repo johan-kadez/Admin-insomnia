18b9e0d6fe47354a3b34abc7290f3090246ab36b
johan-kadez <dafakurniawan1221@gmail.com>
2026-08-16 16:50:00 +0700

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,9 +1,8 @@
-// Gunakan config Web App Firebase yang sama dengan Store.
-export const firebaseConfig = {
-  apiKey: "AIzaSyBbuVzR0657lZaTdaLKn9vUOO0diFs5kcw",
-  authDomain: "player-dashboard-b016a.firebaseapp.com",
-  projectId: "player-dashboard-b016a",
-  storageBucket: "player-dashboard-b016a.firebasestorage.app",
-  messagingSenderId: "627678288754",
-  appId: "1:627678288754:web:7f7e77f84b836fab211beb"
+const firebaseConfig = {
+  apiKey: "AIzaSyBqtkkXvchgWt8FszgaaH1arIO20YILcOg",
+  authDomain: "insomnia-store-f66eb.firebaseapp.com",
+  projectId: "insomnia-store-f66eb",
+  storageBucket: "insomnia-store-f66eb.firebasestorage.app",
+  messagingSenderId: "774381713580",
+  appId: "1:774381713580:web:96879b4f5d33664c168709",
 };
